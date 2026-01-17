40f2e228e43dddfb0d403508126e9a838082a57c
Up <10714589+UpcraftLP@users.noreply.github.com>
2026-01-17 07:42:12 +0100

    🎨 dont register task in afterEvaluate

--- src/main/kotlin/info/hytalemodding/gradle/hytalemod/decompile/_DecompileTask.kt
+++ src/main/kotlin/info/hytalemodding/gradle/hytalemod/decompile/_DecompileTask.kt
@@ -2,11 +2,15 @@ package info.hytalemodding.gradle.hytalemod.decompile
 
 import info.hytalemodding.gradle.hytalemod.HytaleExtension
 import info.hytalemodding.gradle.hytalemod.util.hytale
+import info.hytalemodding.gradle.hytalemod.util.ideaExt
 import org.gradle.api.Project
 import org.gradle.api.tasks.JavaExec
 import org.gradle.kotlin.dsl.dependencies
+import org.jetbrains.gradle.ext.settings
 
 fun Project.registerDecompileTask() {
+    val ideaModel = rootProject.extensions.ideaExt
+    ideaModel.project.settings.generateImlFiles = true
 
     val vf = configurations.create("vineFlowerDependencies") {
         isCanBeConsumed = false
@@ -17,26 +21,24 @@ fun Project.registerDecompileTask() {
         "vineFlowerDependencies"("org.vineflower:vineflower:1.11.2")
     }
 
-    afterEvaluate {
-        val hytaleExtension = project.extensions.hytale
+    tasks.register("decompileServer", JavaExec::class.java) {
+        group = HytaleExtension.TASK_GROUP
 
-        val decompileServerTask = tasks.register("decompileServer", JavaExec::class.java) {
-            group = HytaleExtension.TASK_GROUP
+        val serverJar = project.extensions.hytale.serverJar
+        val sourcesFile = serverJar.map { it.asFile.parentFile.resolve("${it.asFile.nameWithoutExtension}-sources.jar") }
 
-            val serverJar = hytaleExtension.serverJar
-            val sourcesFile = hytaleExtension.serverDir.file("${serverJar.get().asFile.nameWithoutExtension}-src.zip")
+        workingDir = temporaryDir
+        mainClass.set("org.jetbrains.java.decompiler.main.decompiler.ConsoleDecompiler")
+        classpath = vf
 
-            workingDir = temporaryDir
-            mainClass.set("org.jetbrains.java.decompiler.main.decompiler.ConsoleDecompiler")
-            classpath = vf
-
-            args = listOf(
+        argumentProviders.add {
+            listOf(
                 serverJar.get().asFile.absolutePath,
-                sourcesFile.get().asFile.absolutePath
+                sourcesFile.get().absolutePath
             )
-
-            inputs.file(serverJar)
-            outputs.file(sourcesFile)
         }
+
+        inputs.file(serverJar)
+        outputs.file(sourcesFile)
     }
 }
